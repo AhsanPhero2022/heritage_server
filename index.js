f530507ce79ed16e39a091a513531e3ed8c1ab30
Sharif Ahmed <97349171+AhsanPhero2022@users.noreply.github.com>
2024-08-31 15:22:10 +0600

    install and get properties done

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    // Connect to MongoDB
+    // await client.connect();
+    console.log("Connected to MongoDB");
+
+    const db = client.db("sm_technology");
+    const productsCollection = db.collection("properties");
+
+    // User Registration
+
+    // User Login
+
+    app.get("/properties", async (req, res) => {
+      const result = await productsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/category", async (req, res) => {
+      const result = await productsCollection.find().toArray();
+
+      result.sort((a, b) => {
+        if (a.category < b.category) return -1;
+        if (a.category > b.category) return 1;
+        return 0;
+      });
+
+      res.send(result);
+    });
+
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  const serverStatus = {
+    message: "Server is running smoothly",
+    timestamp: new Date(),
+  };
+  res.json(serverStatus);
+});
+
+module.exports = app;
